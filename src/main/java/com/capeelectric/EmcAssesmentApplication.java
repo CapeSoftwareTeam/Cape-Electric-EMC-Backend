8b30725dfb8eaade87aa90f8373700a6928c3604
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-02-11 19:27:53 +0530

    Added code for EMC Logger

--- src/main/java/com/capeelectric/EmcAssesmentApplication.java
+++ src/main/java/com/capeelectric/EmcAssesmentApplication.java
@@ -2,12 +2,18 @@ package com.capeelectric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmcAssesmentApplication {
+public class EmcAssesmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmcAssesmentApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmcAssesmentApplication.class);
 	}
 
 }
