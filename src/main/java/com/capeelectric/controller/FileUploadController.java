5b8c7f569872d3a6723908f2db644f16ca771e2e
Sivaraju <sivaraju@capeindia.net>
2022-02-23 19:32:23 +0530

    added loggers i file store

--- src/main/java/com/capeelectric/controller/FileUploadController.java
+++ src/main/java/com/capeelectric/controller/FileUploadController.java
@@ -2,6 +2,8 @@ package com.capeelectric.controller;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,18 +22,22 @@ import com.capeelectric.service.FileStorageService;
 @RestController
 @RequestMapping("/api/emc/v1")
 public class FileUploadController {
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 	@Autowired
 	private FileStorageService storageService;
 
 	@PostMapping("/upload/{emcId}")
 	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable Integer emcId)
 			throws IOException {
+		logger.debug("File Upload Start");
 		storageService.store(file, emcId);
+		logger.debug("File Upload 	End");
 		return new ResponseEntity<String>("File  Upload Successfully", HttpStatus.OK);
 	}
 
 	@GetMapping("/files/{emcid}")
 	public ResponseEntity<byte[]> getFile(@PathVariable Integer emcid) {
+		logger.debug("Retrieve File Start emcId : {}", emcid);
 		ResponseFile fileDB = storageService.getFile(emcid);
 		return ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDB.getFileName() + "\"")
--- src/main/java/com/capeelectric/service/FileStorageService.java
+++ src/main/java/com/capeelectric/service/FileStorageService.java
@@ -2,6 +2,8 @@ package com.capeelectric.service;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -9,9 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.capeelectric.model.ResponseFile;
 import com.capeelectric.repository.FileDBRepository;
+import com.capeelectric.service.impl.FacilityDataServiceImpl;
 
 @Service
 public class FileStorageService {
+	private static final Logger logger = LoggerFactory.getLogger(FileStorageService.class);
 
 	@Autowired
 	private FileDBRepository fileDBRepository;
@@ -23,6 +27,7 @@ public class FileStorageService {
 		FileDB.setFileName(fileName);
 		FileDB.setData(file.getBytes());
 		FileDB.setFileType(file.getContentType());
+		logger.debug("File Saved In DB");
 		return fileDBRepository.save(FileDB);
 	}
 
